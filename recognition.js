064c94596d10a5d099c7c07b526da3ad3977fd95
Akhsarbek Gozoev <gakhsar@gmail.com>
2022-12-10 16:00:39 +0000

    Index and script added

---
+++ recognition.js
@@ -0,0 +1,92 @@
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+
+
+var recognition = new SpeechRecognition();
+recognition.continuous = true;
+recognition.lang = 'en-US'; //no-NO
+recognition.start();
+
+var diagnostic = document.querySelector('.output');
+
+recognition.onresult = function (event) {
+    var text = event.results[event.results.length - 1][0].transcript;
+    diagnostic.textContent = 'Result received: ' + text + '.';
+    console.log('Confidence: ' + event.results[0][0].confidence);
+    // speak();
+}
+
+
+recognition.onend = function () {
+    // setTimeout(() => {
+    //     recognition.start();
+    // }, 1000);
+}
+
+recognition.onnomatch = function (event) {
+    diagnostic.textContent = "I didn't recognise that color.";
+}
+
+recognition.onerror = function (event) {
+    diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
+}
+
+const synth = window.speechSynthesis;
+const voiceSelect = document.querySelector("select");
+
+let voices = [];
+function populateVoiceList() {
+    voices = synth.getVoices();
+    const selectedIndex =
+        voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
+    voiceSelect.innerHTML = "";
+
+    for (let i = 0; i < voices.length; i++) {
+        const option = document.createElement("option");
+        option.textContent = `${voices[i].name} (${voices[i].lang})`;
+        option.setAttribute("data-lang", voices[i].lang);
+        option.setAttribute("data-name", voices[i].name);
+        voiceSelect.appendChild(option);
+    }
+    voiceSelect.selectedIndex = selectedIndex;
+}
+
+populateVoiceList();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+
+function speak() {
+    if (synth.speaking) {
+        console.error("speechSynthesis.speaking");
+        return;
+    }
+
+    if (diagnostic.textContent !== "") {
+        const utterThis = new SpeechSynthesisUtterance(diagnostic.textContent);
+
+        utterThis.onend = function (event) {
+            console.log("SpeechSynthesisUtterance.onend");
+        };
+
+        utterThis.onerror = function (event) {
+            debugger;
+            console.error("SpeechSynthesisUtterance.onerror");
+        };
+
+        const selectedOption =
+            voiceSelect.selectedOptions[0].getAttribute("data-name");
+
+        for (let i = 0; i < voices.length; i++) {
+            if (voices[i].name === selectedOption) {
+                utterThis.voice = voices[i];
+                break;
+            }
+        }
+        synth.speak(utterThis);
+    }
+}
+
+voiceSelect.onchange = function () {
+    speak();
+};
